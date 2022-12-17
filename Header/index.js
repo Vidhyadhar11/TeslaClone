d3c66fb2a2f32806cf62be3918639e525629d907
Vidhyadhar11 <94471463+Vidhyadhar11@users.noreply.github.com>
2022-12-17 23:07:45 +0530

    Add files via upload

---
+++ Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+const = (props) => {
+    return(
+        <Text>this is not a component</Text>
+    );
+};
+export default ;
