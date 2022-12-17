8f4116747c5808af383d41e9b8d926741c579f57
Vidhyadhar11 <94471463+Vidhyadhar11@users.noreply.github.com>
2022-12-17 22:59:16 +0530

    Add files via upload

---
+++ functioncomponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+const = (props) => {
+    return(
+        <Text>this is not a component</Text>
+    );
+};
+export default ;
+
+
+import{ StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+});
+
+export default styles;
